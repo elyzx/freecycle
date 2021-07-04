0f913a958056975a362b3fadd4dfa9ff7a4575dd
elyzx <ellenchildren@icloud.com>
2021-07-04 12:59:31 +0200

    push to master

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,36 @@ const router = require("express").Router();
 
 const ListingModel = require("../models/Listing.model");
 
+
 /* GET home page */
 router.get("/", (req, res, next) => {
-
-  ListingModel.find()
+  if (req.session.loggedInUser) {
+    ListingModel.find()
     .then((listings) => {
       res.render("index", {listings})
     })
     .catch(() => {
       next('No available listings. Check back later!')
     })
+  }
+  else {
+    res.redirect('/login')
+  }
 });
+
+// router.get("/:id", (req, res, next) => {
+//   if (req.session.loggedInUser) {
+//     ListingModel.find()
+//     .then((listings) => {
+//       res.render("index", {listings})
+//     })
+//     .catch(() => {
+//       next('No available listings. Check back later!')
+//     })
+//   }
+//   else {
+//     res.redirect('/login')
+//   }
+// });
 
 module.exports = router;
--- routes/listing.routes.js
+++ routes/listing.routes.js
@@ -7,61 +7,27 @@ const UserModel = require("../models/User.model");
 
 
 // ---------- VIEW/READ LISTINGS ---------- //
-// Show the homepage - same as ('/')
-// Handle GET request to /listings and render homepage
+// Handle GET request to /listings and redirect to homepage
+router.get('/listings', (req, res, next) => {
+    res.redirect('/')
+});
 
-// router.get('/listings', (req, res, next) => {
-//     res.render('index.hbs')
-// });
-
-// ----------------------- ELLEN ORIGINAL CODE ---------------------------------
-// Dynamically show an individual listing
 // Handle GET request to /listings:id
-
-// router.get('/listings/:id', (req, res, next) => {
-//     let dynamicListingId = req.params.id
-    
-//     ListingModel.findById(dynamicListingId)
-//         .then((listing) => {
-//             res.render('listings/viewListing.hbs', {listing})
-//         })
-//         .catch(() => {
-//             next('Failed to find listing details')
-//         })
-// })
-
-// ----------------------- ELLEN ORIGINAL CODE ---------------------------------
-
-
-// ----------------------- HUMBERTO CODE ---------------------------------
-
-// router.get('/listings', (req, res, next) => {
-
-//     res.render('index.hbs')
-// });
-
 router.get('/listings/:id', (req, res, next) => {
-    let dynamicUserId = req.params.id
+    let dynamicListingId = req.params.id
     
-    UserModel.findById(dynamicUserId)
-        .then((user) => {
-            console.log(user)
-            res.render('listings/viewListing.hbs', {user})
+    ListingModel.findById(dynamicListingId)
+        .then((listing) => {
+            res.render('listings/viewListing.hbs', {listing})
         })
         .catch(() => {
-            next('Failed to find user details')
+            next('Failed to find listing details')
         })
 })
-
-// ----------------------- HUMBERTO CODE ---------------------------------
-
-
-
 
 // Later on -- POST request to handle form submission/contact functionality
 
 // ---------- CREATE LISTINGS ---------- //
-// Render the Create Listing Page with form
 // Handle GET requests to /create listings page
 router.get('/create', (req, res, next) => {
     res.render('listings/createListing.hbs')
@@ -69,13 +35,13 @@ router.get('/create', (req, res, next) => {
 
 // Add form submissions to DB & redirect user to Manage page
 // Handle POST requests to /create listings page 
-router.post('create', (req, res, next) => {
-    const {title, description, neighbourhood} = req.body
+router.post('/create', (req, res, next) => {
+    const {title, description} = req.body
 
     // Add the listing to our DB
-    ListingModel.create({title, description, neighbourhood})
+    ListingModel.create({title, description})
         .then(() => {
-            res.redirect('/manage')
+            res.redirect('/')
         })
         .catch(() => {
             next('Failed to create new listing')
@@ -87,9 +53,18 @@ router.post('create', (req, res, next) => {
 // ---------- UPDATE LISTINGS ---------- //
 // Show the user all their active listings
 // Handle GET request to /manage listings page
-router.get('/manage', (req, res, next) => {
-    res.render('listings/manageListings.hbs')
-});
+router.get('/manage/:id', (req, res, next) => {
+    let dynamicUserId = req.params.id
+    
+    UserModel.findById(dynamicUserId)
+        .then((user) => {
+            console.log(user)
+            res.render('listings/manageListing.hbs', {user})
+        })
+        .catch(() => {
+            next('Failed to find user details')
+        })
+})
 
 // Enable the user to edit an existing listing
 router.get('/edit', (req, res, next) => {
