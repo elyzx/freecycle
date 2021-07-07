f114fb80e6d89669185c7770b25740fd843a0696
Humberto Angelucci <angeluccirincon@gmail.com>
2021-07-07 13:06:30 +0200

    fixed index.js logic for neighbourhood and users - 07.07.21

--- routes/index.js
+++ routes/index.js
@@ -30,21 +30,15 @@ router.get("/", checkLoggedIn,  (req, res, next) => {
       UserModel.findById(req.session.loggedInUser._id)
         .then((user) => {
 
-          
-          
-      //const userMap = user.map(x => x.neighbourhood)
-      //console.log(userMap)
+      const listingsMap = listings.map(x => x) 
+      
 
-      //console.log(user.neighbourhood, 'this is the user neighbourhood')
-
-      const listingsMap = listings.map(x => x.neighbourhood._id);
 
       let matchId = [];
 
       for( let i = 0; i < listingsMap.length; i++ ) {
-        if(listingsMap[i].toString() == user.neighbourhood.toString()){
+        if(listingsMap[i].neighbourhood._id.toString() == user.neighbourhood.toString()){
           matchId.push(listingsMap[i])
-
         }
         console.log(typeof user.neighbourhood, 'this is the user in the for loop')
         console.log(typeof listingsMap[i], 'this is the list in the for loop')
